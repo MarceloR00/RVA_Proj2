e820292c14afcdab910e7fbefcff9842ef718fcf
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-12-08 10:57:04 +0000

    Added moving obstacle.

---
+++ src/Assets/Scripts/Obstacles/MovingWallManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWallManager : MonoBehaviour
+{
+    [SerializeField] float min_y;
+    [SerializeField] float max_y;
+    [SerializeField] float velocity;
+    
+    int direction = -1;
+
+    void Update() {
+        MoveWall();
+    }
+
+    void MoveWall() {
+        UpdatePosition();
+        UpdateDirection();
+    }
+
+    void UpdatePosition() {
+        float new_value_y = transform.position.y + velocity * direction;
+
+        if (new_value_y < min_y) {
+            new_value_y = min_y;
+        }
+        else if (new_value_y > max_y) {
+            new_value_y = max_y;
+        }
+
+        transform.position = new Vector3(transform.position.x, new_value_y, transform.position.z);
+    }
+
+    void UpdateDirection() {
+        if (transform.position.y == min_y || transform.position.y == max_y) {
+            direction *= -1;
+        }
+    }
+}
