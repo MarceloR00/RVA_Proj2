d9f68e777c279793797520fa71306ae006987364
Marcelo Reis <up201809566@fe.up.pt>
2022-12-10 20:21:46 +0000

    Added target movement.

---
+++ src/Assets/Scripts/Target/TargetMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    [SerializeField] Transform[] positions;
+    [SerializeField] float velocity;
+    [SerializeField] Animator animator;
+
+    int current_position_index = 0;
+    float time_took_between_points;
+    float current_start_time_between_points;
+
+    bool blocked = false;
+
+    void Start() {
+        ComputeTimeBetweenPoints();
+    }
+
+    public void Reset() {
+        // reset initial position
+        current_position_index = 0;
+
+        ComputeTimeBetweenPoints();
+    }
+
+    void FixedUpdate() {
+        if (blocked) return;
+
+        if (ReachedTargetPosition()) {
+            StartCoroutine(Rotate());
+        }
+        else {
+            Move();
+        }
+    }
+
+    void Move() {
+        float current_time_between_points = Time.time - current_start_time_between_points;
+        float ratio = current_time_between_points / time_took_between_points;
+        transform.position = Vector3.Lerp(transform.position, positions[current_position_index].position, ratio);
+
+        animator.SetBool("Walk Forward", true);
+    }
+
+    void SetupNextMovement() {
+        NextPositionIndex();
+        ComputeTimeBetweenPoints();
+        current_start_time_between_points = Time.time;
+    }
+
+    void NextPositionIndex() {
+        current_position_index = (current_position_index + 1) % positions.Length;
+    }
+
+    void ComputeTimeBetweenPoints() {
+        float distance_between_points = Vector3.Distance(transform.position, positions[current_position_index].position);
+        time_took_between_points = distance_between_points * velocity;
+    }
+
+    bool ReachedTargetPosition() {
+        return Vector3.Distance(transform.position, positions[current_position_index].position) < 0.5;
+    }
+
+    IEnumerator Rotate() {
+        blocked = true;
+        
+        animator.SetBool("Walk Forward", false);
+        
+        animator.SetTrigger("Jump");
+
+        yield return new WaitForSeconds(0.2f);
+
+        float duration = 0.42f;
+        Quaternion startRotation = transform.rotation ;
+        Quaternion endRotation = Quaternion.Euler( new Vector3(0, -180, 0) ) * startRotation ;
+        for( float t = 0 ; t < duration ; t+= Time.deltaTime )
+        {
+            transform.rotation = Quaternion.Lerp( startRotation, endRotation, t / duration ) ;
+            yield return null;
+        }
+        transform.rotation = endRotation  ;
+        
+        yield return new WaitForSeconds(0.5f);
+
+        SetupNextMovement();
+        
+        blocked = false;
+
+        yield return null;
+    }
+}
